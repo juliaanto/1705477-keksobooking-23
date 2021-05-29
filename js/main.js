1d19bed8b8453f67cdab45788da578d183e6f495
juliaanto <1306577@mail.ru>
2021-05-29 20:16:58 +0300

    Вторая функция

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-function getRandomNumber (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomDecimal (min, max, digits) {
+  return (Math.random() * (max - min) + min).toFixed(digits);
 }
 
-getRandomNumber(2,7);
+function getRandomNumber (min, max) {
+  return getRandomDecimal(min, max, 0);
+}
+
+getRandomNumber(2,8);
