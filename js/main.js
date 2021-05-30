523e162a187567d84b68882669361cd33e513250
juliaanto <1306577@mail.ru>
2021-05-30 12:47:18 +0300

    Добавлена валидация введенных данных

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 function getRandomDecimal (min, max, digits) {
+  if (min >= max) {
+    throw 'Введены некорректные значения. Значение "До" должно быть больше значения "От".';
+  }
+
   return (Math.random() * (max - min) + min).toFixed(digits);
 }
 
