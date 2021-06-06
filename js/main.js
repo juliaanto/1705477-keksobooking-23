8a1c66d37cce118d7cafcb56e8a532aebb4502b2
juliaanto <1306577@mail.ru>
2021-06-06 17:55:17 +0300

    Генерация данных

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const SIMILAR_ANNOUNCEMENTS = 10;
+
 /**
  * Возвращает случайное число с плавающей точкой из переданного положительного диапазона
  *
@@ -25,4 +56,99 @@ function getRandomNumber (min, max) {
   return getRandomDecimal(min, max, 0);
 }
 
-getRandomNumber(2,8);
+/**
+ * Поиск случайного элемента в переданном массиве
+ *
+ * @param {*} elements - массив
+ */
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+/**
+ * Создает массив случайной длины на основе переданного массива
+ *
+ * @param {*} array - исходный массив
+ * @return {*}
+ */
+const getRandomArray = (array) => {
+
+  const currentArray = [];
+  const elementsCount = getRandomNumber(1,[array.length]);
+
+  for (let currentElement = 0; currentElement < elementsCount - 1; currentElement++) {
+    const isActiveNumber = getRandomNumber(0,1);
+    let isActive = false;
+
+    if (isActiveNumber === '1') {
+      isActive = true;
+    }
+
+    if (isActive) {
+      currentArray.push(array[currentElement]);
+    }
+  }
+  return currentArray;
+};
+
+/**
+ * Создает автора
+ *
+ * @return {*}
+ */
+const createAutor = () => {
+  const author = {
+    avatar: `img/avatars/user0${getRandomNumber(1,8)}.png`,
+  };
+  return author;
+};
+
+/**
+ * Создает местоположение в виде географических координат
+ *
+ * @return {*}
+ */
+const createLocation = () => {
+  const location = {
+    lat: getRandomDecimal(35.65000, 35.70000, 5),
+    lng: getRandomDecimal(139.70000, 139.80000, 5),
+  };
+  return location;
+};
+
+/**
+ * Создает предложение (которое содержит информацию об объявлении)
+ *
+ * @return {*}
+ */
+const createOffer = () => {
+  const offer = {
+    title: 'Отель Duquesne Eiffel',
+    address: Object.values(createLocation()).join(', '),
+    price: getRandomNumber(1000, 100000),
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomNumber(1, 20),
+    guests: getRandomNumber(1, 20),
+    checkin: getRandomArrayElement(TIMES),
+    checkout: getRandomArrayElement(TIMES),
+    features: getRandomArray(FEATURES),
+    description: 'Отель Duquesne Eiffel расположен в Париже, всего в 5 минутах ходьбы от парка Марсово поле. В отеле есть звукоизолированные номера с бесплатным Wi-Fi, а из окон некоторых номеров открывается вид на Эйфелеву башню.',
+    photos: getRandomArray(PHOTOS),
+  };
+  return offer;
+};
+
+/**
+ *  Создает объявление
+ *
+ * @return {*}
+ */
+const createAnnouncement = () => {
+  const announcement = {
+    author: createAutor(),
+    offer: createOffer(),
+    location: createLocation(),
+  };
+  return announcement;
+};
+
+const similarAnnouncements = new Array(SIMILAR_ANNOUNCEMENTS).fill(null).map(() => createAnnouncement());
+similarAnnouncements();
