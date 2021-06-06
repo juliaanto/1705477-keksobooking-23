5e16b6ccb1ca16bc80c7d9d007bd726fce7112c2
juliaanto <1306577@mail.ru>
2021-06-06 12:23:58 +0300

    Добавление комментариев jsDoc для функций

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+/**
+ * Возвращает случайное число с плавающей точкой из переданного положительного диапазона
+ *
+ * @param {*} min - начало диапазона
+ * @param {*} max - конец диапазона
+ * @param {*} digits - количество знаков после запятой
+ * @return {*}
+ */
 function getRandomDecimal (min, max, digits) {
   if (min >= max) {
     throw 'Введены некорректные значения. Значение "До" должно быть больше значения "От".';
@@ -6,6 +14,13 @@ function getRandomDecimal (min, max, digits) {
   return (Math.random() * (max - min) + min).toFixed(digits);
 }
 
+/**
+ * Возвращает случайное целое число из переданного положительного диапазона
+ *
+ * @param {*} min - начало диапазона
+ * @param {*} max - конец диапазона
+ * @return {*}
+ */
 function getRandomNumber (min, max) {
   return getRandomDecimal(min, max, 0);
 }
