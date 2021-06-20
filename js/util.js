e56ba60d57cd4b1895bf6ad1a9c65606f674a2c8
juliaanto <1306577@mail.ru>
2021-06-20 12:55:17 +0300

    Генерация разметки похожих объявлений

---
+++ js/util.js
@@ -0,0 +1,76 @@
+const hideEmptyBlock = (announcement, element) => {
+  if (announcement.author.avatar === undefined) {
+    element.querySelector('.popup__avatar').classList.add('hidden');
+  }
+
+  if (announcement.offer.price === undefined) {
+    element.querySelector('.popup__text--price').classList.add('hidden');
+  }
+
+  if (announcement.offer.rooms === undefined || announcement.offer.guests === undefined) {
+    element.querySelector('.popup__text--capacity').classList.add('hidden');
+  }
+
+  if (announcement.offer.checkin === undefined || announcement.offer.checkout === undefined) {
+    element.querySelector('.popup__text--time').classList.add('hidden');
+  }
+
+  if (announcement.offer.features === undefined) {
+    element.querySelector('.popup__feature').classList.add('hidden');
+  }
+
+  if (announcement.offer.photos === undefined) {
+    element.querySelector('.popup__photos').classList.add('hidden');
+  }
+};
+
+const getOfferType = (offerType) => {
+  let offerTypeValue = '';
+  if (offerType === 'flat') {
+    offerTypeValue = 'Квартира';
+  } else if (offerType === 'bungalow') {
+    offerTypeValue = 'Бунгало';
+  } else if (offerType === 'house') {
+    offerTypeValue = 'Дом';
+  } else if (offerType === 'palace') {
+    offerTypeValue = 'Дворец';
+  } else if (offerType === 'hotel') {
+    offerTypeValue = 'Отель';
+  }
+  return offerTypeValue;
+};
+
+const getFeatures = (announcement, element) => {
+  const featuresList = element.querySelector('.popup__features');
+  const announcementFeatures = announcement.offer.features;
+
+  while (featuresList.firstChild) {
+    featuresList.removeChild(featuresList.firstChild);
+  }
+
+  if (announcementFeatures !== undefined) {
+    announcementFeatures.forEach((feature) => {
+      const newFeature = document.createElement('li');
+      newFeature.classList.add('popup__feature');
+      newFeature.classList.add(`popup__feature--${feature}`);
+      featuresList.appendChild(newFeature);
+    });
+  }
+};
+
+const getPhotos = (announcement, element) => {
+  const photosList = element.querySelector('.popup__photos');
+  const photoTemplate = element.querySelector('.popup__photo');
+  const announcementPhotos = announcement.offer.photos;
+  photosList.removeChild(photoTemplate);
+
+  if (announcementPhotos !== undefined) {
+    announcementPhotos.forEach((photo) => {
+      const photoElement = photoTemplate.cloneNode(true);
+      photoElement.src = photo;
+      photosList.appendChild(photoElement);
+    });
+  }
+};
+
+export {hideEmptyBlock, getOfferType, getFeatures, getPhotos};
