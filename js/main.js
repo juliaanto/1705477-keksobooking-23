f3a235483c4c477ebd2f645d59a47926327cc98b
juliaanto <1306577@mail.ru>
2021-06-26 19:35:02 +0300

    Переключение режимов страницы

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 import {createSimilarAnnouncements} from './mock/data.js';
 import {renderCard} from './similar-announcement-list.js';
+import {disablePage, activatePage} from './ad-form.js';
 
 const SIMILAR_ANNOUNCEMENTS = 10;
 const similarAnnouncements = createSimilarAnnouncements(SIMILAR_ANNOUNCEMENTS);
 renderCard(similarAnnouncements[0]);
+
+disablePage();
+activatePage();
