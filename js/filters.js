d12a21c5d9232e16ef486bbdf768e6657b440059
juliaanto <1306577@mail.ru>
2021-07-16 22:00:58 +0300

    Фильтр по удобствам

--- js/filters.js
+++ js/filters.js
@@ -8,6 +8,8 @@ const housingType = mapFilters.querySelector('#housing-type');
 const housingPrice = mapFilters.querySelector('#housing-price');
 const housingRooms = mapFilters.querySelector('#housing-rooms');
 const housingGuests = mapFilters.querySelector('#housing-guests');
+const housingFeatures = mapFilters.querySelector('#housing-features');
+
 
 /** Переводит фильтры в неактивное состояние */
 const disableFilters = () => {
@@ -56,14 +58,34 @@ const getHousingGuests = (guests) => {
   }
 };
 
+const getFeatures = (announcementFeatures) => {
+  const checkedFeatureElements = housingFeatures.querySelectorAll('input[type="checkbox"]:checked');
+  let isFeatures = true;
+
+  if (checkedFeatureElements.length === 0) {
+    isFeatures = true;
+  } else if (announcementFeatures === undefined) {
+    isFeatures = false;
+  } else {
+    checkedFeatureElements.forEach((feature) => {
+      if (!announcementFeatures.includes(feature.value)) {
+        isFeatures = false;
+      }
+    });
+  }
+
+  return isFeatures;
+};
+
 const applyFilters = (announcement) => {
 
   const isHousingType = housingType.value === 'any' || housingType.value === announcement.offer.type;
   const isHousingPrice = housingPrice.value === 'any' || housingPrice.value === getHousingPrice(announcement.offer.price);
   const isHousingRooms = housingRooms.value === 'any' || housingRooms.value === String(announcement.offer.rooms);
-  const ishousingGuests = housingGuests.value === 'any' || housingGuests.value === getHousingGuests(announcement.offer.guests);
+  const isHousingGuests = housingGuests.value === 'any' || housingGuests.value === getHousingGuests(announcement.offer.guests);
+  const isHousingFeatures = getFeatures(announcement.offer.features);
 
-  return isHousingType && isHousingPrice && isHousingRooms && ishousingGuests;
+  return isHousingType && isHousingPrice && isHousingRooms && isHousingGuests && isHousingFeatures;
 };
 
 /**
@@ -88,8 +110,6 @@ const initHousingTypeChange = (announcements) => {
     removePins();
     filterAndAddPinsToMap(announcements);
   });
-
 };
-
 
 export {disableFilters, enableFilters, resetFilters, filterAndAddPinsToMap, initHousingTypeChange};
